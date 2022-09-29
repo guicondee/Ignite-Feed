05efa28d31ec6f1694d85b22f10f7d32ea08813f
guicondee <guicondee@outlook.com>
2022-09-29 19:55:46 -0300

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import { Header } from "./Components/Header/Header"
 import { Sidebar } from "./Components/Sidebar/Sidebar"
 import { Widgets } from "./Components/widgtes/Widgets"
+import { Comment } from "./Components/Comment/Comment"
 
 import global from './global.css'
 import styles from './App.module.css'
-import { Comment } from "./Components/Comment/Comment"
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'http://guicondee.github.io/Dr/' },
+    ],
+    publishedAt: new Date('2022-09-27 21:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/gustavowebjs.png',
+      name: 'Gustavo Vinicios',
+      role: 'CTO @Rampp'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal 👋' },
+      { type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻' },
+      { type: 'link', content: 'http://web.rampp.com.br/' },
+    ],
+    publishedAt: new Date('2022-09-28 15:17')
+  },
+]
+
 
 export function App() {  
   return (
@@ -13,8 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-           <Widgets />
-           <Widgets />
+          {posts.map(post => {
+            return (
+              <Widgets 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+           })}
         </main>
       </div>
     </div>
--- src/Components/widgtes/Widgets.jsx
+++ src/Components/widgtes/Widgets.jsx
@@ -1,33 +1,47 @@
 import styles from './Widgets.module.css'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR'
 
 import  guiProfile  from '../../assets/guiProfile.jpg'
 import { Comment } from '../Comment/Comment'
 import { Avatar } from '../Avatar/Avatar'
 
-export function Widgets() {
+export function Widgets({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBr,
+  })
+
+  const publishedDateRelativeToNow =  formatDistanceToNow(publishedAt, {
+    locale: ptBr,
+    addSuffix: true
+  })
+
   return (
      <main className={styles.widgets}>
         <header>
           <div className={styles.author}>
-            <Avatar hasBorder={true} src='http://github.com/gustavowebjs.png' />
-
+            <Avatar hasBorder={true} src={ author.avatarUrl} />
             <div className={styles.authorInfo}>
-              <strong>Guilherme Condé</strong>
-              <span>Dev Front-End</span>
+              <strong>{author.name}</strong>
+              <span>{author.role}</span>
             </div>
           </div>
          
-          <time title='24 de Setembro às 20:19h' dataTime="2022-09-24 20:15">Públicado há 1 hora</time>
+          <time title={publishedDateFormatted} dataTime={publishedAt.toISOString()}>         
+            {publishedDateRelativeToNow}
+          </time>
         </header>
 
         <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-
-         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-          <p><a target="blank" href="http://guicondee.github.io/Dr/"> 👉 Guilherme.design/doctorcare</a></p>
-
-          <p><a href="">#novoprojeto</a> <a href="">#nlw</a> <a href=""> #rocketseat</a></p>
+          {content.map(line => {
+            if (line.type === 'paragraph') {
+              return (
+                <p>{line.content}</p>
+              )
+            } else if (line.type === 'link') {
+              return <p><a href="#">{line.content}</a></p>
+            }
+          })}
         </div>
 
         <form className={styles.commentForm}>
