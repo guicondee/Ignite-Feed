cb34ed2d4680ae96c9d982ad926a8f4048e52f9a
guicondee <guicondee@outlook.com>
2022-10-02 16:44:25 -0300

    create key

--- src/Components/widgtes/Widgets.jsx
+++ src/Components/widgtes/Widgets.jsx
@@ -54,10 +54,10 @@ export function Widgets({ author, publishedAt, content }) {
           {content.map(line => {
             if (line.type === 'paragraph') {
               return (
-                <p>{line.content}</p>
+                <p key={line.content}>{line.content}</p>
               )
             } else if (line.type === 'link') {
-              return <p><a href="#">{line.content}</a></p>
+              return <p key={line.content}><a href="#">{line.content}</a></p>
             }
           })}
         </div>
@@ -78,7 +78,7 @@ export function Widgets({ author, publishedAt, content }) {
 
         <div className={styles.commentList}>
           {comments.map(comment => {
-            return <Comment content={comment} />
+            return <Comment key={comment} content={comment} />
           })}
         </div>
      </main>
