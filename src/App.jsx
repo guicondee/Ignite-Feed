4c7924019708b40bb855da0bc9c4bd1adcdafb02
guicondee <guicondee@outlook.com>
2022-10-02 16:44:05 -0300

    create key

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,7 @@ export function App() {
           {posts.map(post => {
             return (
               <Widgets 
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
